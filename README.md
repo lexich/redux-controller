02992c9e3648fc9099bccb71debb5ad862c3cbc7
Efremov Alexey <lexich121@gmail.com>
2016-02-04 15:52:59 +0300

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+### About redux-controller
+[![Build Status](https://travis-ci.org/lexich/redux-controller.svg)](https://travis-ci.org/lexich/redux-controller)
+[![NPM version](https://badge.fury.io/js/redux-controller.svg)](http://badge.fury.io/js/redux-controller)
+[![Coverage Status](https://coveralls.io/repos/lexich/redux-controller/badge.png?branch=master)](https://coveralls.io/r/lexich/redux-controller?branch=master)
---
+++ index.js
@@ -0,0 +1,50 @@
+module.exports = function() {
+  var listOfControllers = {}, cache = {};
+  let store;
+
+  function use(_store) {
+    store = _store;
+  }
+
+  function create(name, creator) {
+    if (!name) {
+      throw new Error("name argument isn't define");
+    }
+    if (listOfControllers[name]) {
+      throw new Error("argument name=" + name + " has been already used");
+    }
+
+    if (!(creator instanceof Function)) {
+      throw new Error("creator isn't function");
+    }
+    listOfControllers[name] = creator;
+    return name;
+  }
+
+  function remove(name) {
+    var ptr = listOfControllers[name];
+    listOfControllers[name] = (void 0);
+    cache[name] = (void 0);
+    return !ptr;
+  }
+
+  function get(name) {
+    if (!store) {
+      throw new Error("store isn't define you should call `use` method");
+    }
+    if (!listOfControllers[name])  {
+      throw new Error("controller " + name + " wasn't defined");
+    }
+    var ref = cache[name] || (
+      cache[name] = listOfControllers[name](store.dispatch, store.getState, cache)
+    );
+    return ref;
+  }
+
+  return {
+    use: use,
+    create: create,
+    remove: remove,
+    get: get
+  };
+};
